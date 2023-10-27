d104642a07fecb36177bc39439d064b5abf1a1a3
Lugia808 <naelsilva808@gmail.com>
2023-10-26 23:07:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="title">Buscador CEP</h2>
+      <div className='containerInput'>
+        <input type="text"
+          placeholder="Digite seu CEP..."
+        />
+
+        <button className="buttonSearch"><FiSearch size={25} color='#000' /></button>
+      </div>
+      <main className='main'>
+        <h3>CEP: 8232323</h3>
+        <span>Rua teste</span>
+        <span>Complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande - MS</span>
+      </main>
+
     </div>
+
+
+
   );
 }
 
