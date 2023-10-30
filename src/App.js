238a110781966f333a36205b890e7449200553cc
Lugia808 <naelsilva808@gmail.com>
2023-10-29 23:53:11 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import './styles.css'
+import api from './services/api';
+
+const zipCodeMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g, '')
+  value = value.replace(/(\d{5})(\d)/, '$1-$2')
+  return value
+}
 
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState('');
+  //input chama o valor do item
+  //setInput "seta" um valor novo
+
+  const handleZipCode = (event) => {
+    const { value } = event.target;
+    const formattedValue = zipCodeMask(value);
+    setInput(formattedValue)
+  }
+
+  async function handleSearch() {
+    //01001000/json/
+
+    if (input === '') {
+      alert('envie algum CEP')
+      return
+    }
+
+    try {
+      const response = await api.get(`${input}/json/`)
+      console.log(response.data)
+      setCep(response.data)
+      setInput('')
+    }
+    catch {
+      alert('erro ao buscar')
+      setInput('');
+    }
+
+  }
+
   return (
     <div className="container">
       <h2 className="title">Buscador CEP</h2>
       <div className='containerInput'>
-        <input type="text"
+        <input
+          type="text"
           placeholder="Digite seu CEP..."
+          id="cepText"
+          value={input}
+          onChange={handleZipCode} // Use apenas o evento onChange que chama handleZipCode
+          maxLength={9}
         />
 
-        <button className="buttonSearch"><FiSearch size={25} color='#000' /></button>
+
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#000' />
+        </button>
       </div>
-      <main className='main'>
-        <h3>CEP: 8232323</h3>
-        <span>Rua teste</span>
-        <span>Complemento</span>
-        <span>Vila Rosa</span>
-        <span>Campo Grande - MS</span>
-      </main>
+      {Object.keys(cep).length > 0 && (<main className='main'>
+        <h3 id='cepv'>CEP: {cep.cep}</h3>
+        <span>Rua {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep.localidade} - {cep.uf}</span>
+      </main>)}
+
+
+
 
     </div>
 
